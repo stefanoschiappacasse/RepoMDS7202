57c1ae4937ba810e1153cee5dfa2907d5645da29
Stefano Schiappacasse <stefano.schiappacasse.m@gmail.com>
2021-08-25 17:45:54 -0400

    comentarioFeature

--- prueba.py
+++ prueba.py
@@ -1,12 +1,11 @@
 def funcionPrueba(a, b) -> int:
-<<<<<<< HEAD
     print(f'Prueba resultado:\n{a+b}')
-=======
-    print(f'La suma es:\n{a+b}')
->>>>>>> feature2
+    print(f'Prueba resultado:\n{a+b}')
+    print(f'Prueba resultado:\n{a+b}')
     return(a+b)
     # return(a+b)
 
+assert funcionPrueba(1,2) == 2, 'Error al sumar'
 assert funcionPrueba(1,2) == 2, 'Error al sumar'
 
 
